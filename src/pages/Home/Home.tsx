3e0a0f03b7df7890fc5fa9196dd33aaad0092bf5
Ali <alinksht@gmail.com>
2025-01-24 14:32:33 +0330

    add new rules for firestore

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,8 +1,50 @@
+import { useEffect, useState } from "react";
+import { collection, getDocs } from "firebase/firestore";
+import { db } from "../../config/firebaseConfig";
 
 const Home = () => {
-  return (
-    <div>Home</div>
-  )
-}
+  const [blogs, setBlogs] = useState([]);
 
-export default Home
+  useEffect(() => {
+    const fetchBlogs = async () => {
+      try {
+        const querySnapshot = await getDocs(collection(db, "Blogs"));
+        const blogsData = querySnapshot.docs.map((doc) => ({
+          id: doc.id,
+          ...doc.data(),
+        }));
+        setBlogs(blogsData);
+        console.log(blogsData);
+      } catch (error) {
+        console.error("Error fetching blogs:", error);
+      }
+    };
+
+    
+    fetchBlogs();
+  }, []);
+
+  return (
+    <div>
+      <h1>Blogs</h1>
+      {blogs.map((blog) => (
+        <div key={blog.id}>
+          <h2>{blog.title}</h2>
+          <p>{blog.content}</p>
+          <p>
+            <strong>Author:</strong> {blog.author}
+          </p>
+          <p>
+            <strong>Tags:</strong> {blog.tags.join(", ")}
+          </p>
+          <p>
+            <strong>Created At:</strong>{" "}
+            {new Date(blog.createdAt.seconds * 1000).toLocaleString()}
+          </p>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default Home;
